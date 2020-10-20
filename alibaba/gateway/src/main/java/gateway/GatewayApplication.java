6c51dbb6245d07365c25e849380417b72994b10a
songcx@cloudscope.cn <songcx@cloudscope.cn>
2020-10-20 17:55:50 +0800

    gateway相关代码提交

---
+++ alibaba/gateway/src/main/java/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Description: 网关模块启动类
+ *
+ * @author songcx
+ * @date 2020/10/20 14:08
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class);
+    }
+}
